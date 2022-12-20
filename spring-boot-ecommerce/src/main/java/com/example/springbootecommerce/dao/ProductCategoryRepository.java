8cb2c316b424c0b8ad9a18ae77080965802fb683
wasifali591 <wasifali591@gmail.com>
2022-12-21 00:46:39 +0530

    product list in angular

--- spring-boot-ecommerce/src/main/java/com/example/springbootecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/com/example/springbootecommerce/dao/ProductCategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.springbootecommerce.dao;
 
 import com.example.springbootecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
